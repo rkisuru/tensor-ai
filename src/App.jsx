7173aa90b482ca8934ae3eaf60a9249e8b76117a
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-11-11 23:47:50 +0530

    init google authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Main from "./components/Main/Main";
+import Login from "./components/Login/Login";
 
 const App = () => {
   return (
     <>
+      <Login />
       <Sidebar />
       <Main />
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ContextProvider from "./Context/Context.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>
+  <GoogleOAuthProvider clientId="391923454039-f30anpmt19c5e1k9i9b0df87fqf853i1.apps.googleusercontent.com">
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </GoogleOAuthProvider>
 );
