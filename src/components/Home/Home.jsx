db381845ef7fc823a44284d3704ad6549787ec4d
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-11-13 22:19:58 +0530

    implement Home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from "../Main/Main";
+import SideBar from "../SideBar/SideBar";
+
+const Home = () => {
+  return (
+    <>
+      <SideBar />
+      <Main />
+    </>
+  );
+};
+
+export default Home;
